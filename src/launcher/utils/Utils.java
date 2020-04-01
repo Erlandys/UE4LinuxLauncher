969eee9f60103d1f1fede171ae181b2507e7b65b
ben-mkiv <advoztyerusdowranhivcin2@gmail.com>
2020-04-02 00:56:37 +0200

    fixed resource loader not working when running outside of dev environment

--- src/launcher/utils/Utils.java
+++ src/launcher/utils/Utils.java
@@ -1,12 +1,10 @@
 package launcher.utils;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Random;
+import java.util.*;
 
 public class Utils {
 	public static String randomString(int length) {
@@ -32,27 +30,56 @@ public class Utils {
 
 	// cached resource loader, used to parse HTML files
 	static HashMap<String, String> resourceCache = new HashMap<>();
-	public static String getResource(String path){
 
+	public static String getResource(String path){
 		if(resourceCache.containsKey(path))
 			return resourceCache.get(path);
 
 		String data = "";
-		File file = new File(Utils.class.getClassLoader().getResource(path).getFile());
 
-		if(file.exists()){
-			try {
-				for(String line : Files.readAllLines(file.toPath()))
-					data+=line;
-
-			} catch (Exception ex){
-				System.out.println("failed to load html resource");
-			}
-
-			resourceCache.put(path, data);
+		Class clazz = null;
+		try {
+			clazz = Class.forName("launcher.Main");
+		} catch(Exception ex){
+			ex.printStackTrace();
+			return "";
 		}
 
-		return data;
+		if(clazz != null) try {
+			for (String line : readResourceLocation(clazz, path))
+				data += line;
+		} catch (NullPointerException ex){
+			ex.printStackTrace();
+			return "";
+		}
 
+		if(data.length() > 0)
+			resourceCache.put(path, data);
+
+		return data;
+	}
+
+
+	public static ArrayList<String> readResourceLocation(Class modClazz, String location){
+		InputStream is = null;
+		try {
+			is = modClazz.getClassLoader().getResourceAsStream(location);
+			final BufferedReader reader = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF8")));
+			final ArrayList<String> lines = new ArrayList<>();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				lines.add(line);
+			}
+			return lines;
+		} catch (Throwable ignored) {
+			return null;
+		} finally {
+			if (is != null) {
+				try {
+					is.close();
+				} catch (IOException ignored) {
+				}
+			}
+		}
 	}
 }
